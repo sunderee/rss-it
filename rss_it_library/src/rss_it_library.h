af4cb8e21bf715af5bb0b80582618b1f726e2064
Peter Aleksander Bizjak <peter.aleksander@bizjak.dev>
2025-05-18 10:02:41 +0200

    Create Dart bindings for RSS feed validation

--- rss_it_library/src/rss_it_library.h
+++ rss_it_library/src/rss_it_library.h
@@ -1,10 +1,9 @@
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
-#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
 
 #define FFI_PLUGIN_EXPORT
 
-FFI_PLUGIN_EXPORT int validateFeed(const char* feed_url);
-FFI_PLUGIN_EXPORT char* parseFeeds(const char* feed_urls);
+FFI_PLUGIN_EXPORT bool validateFeedURL(const char* feed_url);
+// FFI_PLUGIN_EXPORT char* parseFeeds(const char* feed_urls);
