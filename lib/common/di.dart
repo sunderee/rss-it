3d257b4e5ff2d903d9b94bacc8ddd54cd016bd5b
Peter Aleksander Bizjak <peter.aleksander@bizjak.dev>
2025-05-18 12:38:00 +0200

    Minor organization

--- lib/common/di.dart
+++ lib/common/di.dart
@@ -2,6 +2,12 @@ import 'dart:io';
 
 import 'package:hive_ce/hive.dart';
 import 'package:rss_it/domain/data/feed_url.entity.dart';
+import 'package:rss_it/domain/providers/db_provider.dart';
+import 'package:rss_it/domain/providers/default_rss_library_provider.dart';
+import 'package:rss_it/domain/providers/hive_db_provider.dart';
+import 'package:rss_it/domain/providers/rss_library_provider.dart';
+import 'package:rss_it/domain/repositories/default_feed_repository.dart';
+import 'package:rss_it/domain/repositories/feed_repository.dart';
 import 'package:rss_it/hive/hive_registrar.g.dart';
 
 Future<void> initializeDependencies() async {
@@ -12,4 +18,17 @@ Future<void> initializeDependencies() async {
 
   // Open HiveCE boxes
   final feedURLBox = await Hive.openBox<FeedURLEntity>('feed_url');
+
+  // Providers
+  final DBProvider dbProviderInstance = HiveDBProvider(
+    feedURLsBoxInstance: feedURLBox,
+  );
+  final RssLibraryProvider rssLibraryProviderInstance =
+      DefaultRSSLibraryProvider();
+
+  // Repositories
+  final FeedRepository feedRepositoryInstance = DefaultFeedRepository(
+    dbProviderInstance: dbProviderInstance,
+    rssLibraryProviderInstance: rssLibraryProviderInstance,
+  );
 }
