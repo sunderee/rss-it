4574d3d4b1bf73e5294a80445b7f4a98ba7f5105
Peter Aleksander Bizjak <peter.aleksander@bizjak.dev>
2025-05-23 08:27:14 +0200

    Refactor RSS library to remove custom feed models and streamline parsing
    - Removed custom Feed and FeedItem classes to simplify the codebase.
    - Updated `parseFeedURLs` and `validateFeedURL` functions to utilize Protobuf for data handling.
    - Enhanced memory management by ensuring proper allocation and deallocation with C interop.
    - Adjusted parsing logic to work with Protobuf responses instead of JSON.

--- rss_it_library/lib/rss_it_library.dart
+++ rss_it_library/lib/rss_it_library.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:convert';
 import 'dart:ffi';
 import 'dart:io';
 
@@ -7,90 +6,6 @@ import 'package:ffi/ffi.dart';
 import 'package:rss_it_library/protos/feed.pb.dart';
 
 import 'rss_it_library_bindings_generated.dart';
-
-// Custom models for RSS feeds
-class FeedItem {
-  final String title;
-  final String? description;
-  final String? link;
-  final String? image;
-
-  FeedItem({required this.title, this.description, this.link, this.image});
-
-  factory FeedItem.fromJson(Map<String, dynamic> json) {
-    return FeedItem(
-      title: json['title'] as String,
-      description: json['description'] as String?,
-      link: json['link'] as String?,
-      image: json['image'] as String?,
-    );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'title': title,
-      'description': description,
-      'link': link,
-      'image': image,
-    };
-  }
-}
-
-class Feed {
-  final String url;
-  final String title;
-  final String? description;
-  final String? image;
-  final List<FeedItem> items;
-
-  Feed({
-    required this.url,
-    required this.title,
-    this.description,
-    this.image,
-    required this.items,
-  });
-
-  factory Feed.fromJson(Map<String, dynamic> json) {
-    return Feed(
-      url: json['url'] as String,
-      title: json['title'] as String,
-      description: json['description'] as String?,
-      image: json['image'] as String?,
-      items: (json['items'] as List<dynamic>)
-          .map((item) => FeedItem.fromJson(item as Map<String, dynamic>))
-          .toList(),
-    );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'url': url,
-      'title': title,
-      'description': description,
-      'image': image,
-      'items': items.map((item) => item.toJson()).toList(),
-    };
-  }
-}
-
-class ParseFeedsResult {
-  final List<Feed> feeds;
-  final List<String> errors;
-
-  ParseFeedsResult({required this.feeds, required this.errors});
-
-  factory ParseFeedsResult.fromJson(Map<String, dynamic> json) {
-    return ParseFeedsResult(
-      feeds: (json['feeds'] as List<dynamic>)
-          .map((feed) => Feed.fromJson(feed as Map<String, dynamic>))
-          .toList(),
-      errors: (json['errors'] as List<dynamic>)
-          .map((error) => error as String)
-          .toList(),
-    );
-  }
-}
 
 Future<bool> validateFeedURL(String url) async {
   final data = ValidateFeedRequest(url: url);
@@ -109,7 +24,6 @@ Future<bool> validateFeedURL(String url) async {
     final rawValidationResult = _bindings.validate(cData, dataBuffer.length);
 
     // Convert C Pointer<Char> back to Dart List<int>
-    // We need to know the length - assuming it's null-terminated or we need to modify C code to return length
     final List<int> resultData = [];
     int i = 0;
     while (rawValidationResult[i] != 0 && i < 1024) {
@@ -127,24 +41,24 @@ Future<bool> validateFeedURL(String url) async {
   }
 }
 
-Future<ParseFeedsResult> parseFeedURLs(List<String> urls) async {
-  // Create JSON request
-  final requestJson = jsonEncode({'urls': urls});
-  final requestBytes = utf8.encode(requestJson);
+Future<ParseFeedsResponse> parseFeedURLs(List<String> urls) async {
+  // Create protobuf request
+  final data = ParseFeedsRequest(urls: urls);
+  final dataBuffer = data.writeToBuffer();
 
-  // Convert Dart JSON to C Pointer<Char>
-  final Pointer<Char> cData = malloc<Char>(requestBytes.length);
+  // Convert Dart Uint8List to C Pointer<Char>
+  final Pointer<Char> cData = malloc<Char>(dataBuffer.length);
 
   // Copy data from Dart buffer to C memory
-  for (int i = 0; i < requestBytes.length; i++) {
-    cData[i] = requestBytes[i];
+  for (int i = 0; i < dataBuffer.length; i++) {
+    cData[i] = dataBuffer[i];
   }
 
   try {
     // Call the C function
-    final rawParseResult = _bindings.parse(cData, requestBytes.length);
+    final rawParseResult = _bindings.parse(cData, dataBuffer.length);
 
-    // Convert C Pointer<Char> back to Dart String
+    // Convert C Pointer<Char> back to Dart List<int>
     final List<int> resultData = [];
     int i = 0;
     while (rawParseResult[i] != 0 && i < 10240) {
@@ -153,11 +67,10 @@ Future<ParseFeedsResult> parseFeedURLs(List<String> urls) async {
       i++;
     }
 
-    // Convert bytes to string and parse JSON
-    final resultString = utf8.decode(resultData);
-    final resultJson = jsonDecode(resultString) as Map<String, dynamic>;
+    // Parse protobuf response
+    final parseResult = ParseFeedsResponse.fromBuffer(resultData);
 
-    return ParseFeedsResult.fromJson(resultJson);
+    return parseResult;
   } finally {
     // Always free the allocated memory
     malloc.free(cData);
